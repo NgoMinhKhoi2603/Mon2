8ad37c343a98c3da4d7ace97a536d1960d46144f
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2022-09-09 20:42:06 +0700

    assignmentJS3

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,13 @@ function update() {
     var xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
         var data = JSON.parse(this.responseText);
-
-        for(i=0;i<3;i++){
-            var t = data.data[i].name;
-            var x1 = document.getElementById("name"+i);
-            x1.innerText = t;
+        var list = data.data;
+        for(var i=0;i<list.length;i++){
+            var name = list[i].name;
+            var str = "<li>"+name+"</li>";
+            var x = document.getElementById("menu");
+            x.innerHTML += str;
         }
-
     }
     var link = "https://foodgroup.herokuapp.com/api/menu";
     xhttp.open("get", link, true);
